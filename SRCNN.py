de93e0e089a5d822d4361cefe74b786787604578
Tianjian Cai <tjcaiforapp@gmail.com>
2018-03-08 22:17:17 -0500

    updates (can work)

--- SRCNN.py
+++ SRCNN.py
@@ -11,35 +11,52 @@ import Layers
 import psnr
 
 
-BIT_DEEPTH = 8
-files_X = ['img/X/'+str for str in sorted(os.listdir('img/X/'))]
-files_Y = ['img/Y/'+str for str in sorted(os.listdir('img/Y/'))]
+BIT_DEPTH = 8
+RESIZE_K = 2
+LEARNING_RATE = 0.001
 
-img_raw_X = [tf.read_file(file) for file in files_X]
-img_raw_Y = [tf.read_file(file) for file in files_Y]
-img_decoded_X = [tf.cast(tf.image.decode_jpeg(i),tf.float32)/(2**BIT_DEEPTH) for i in img_raw_X]
-img_decoded_Y = [tf.cast(tf.image.decode_jpeg(i),tf.float32)/(2**BIT_DEEPTH) for i in img_raw_Y]
-img_resized_X = [tf.image.resize_images(i,[tf.shape(tf.image.decode_jpeg(img_raw_Y[0]))[0],tf.shape(tf.image.decode_jpeg(img_raw_Y[0]))[1]]) for i in img_decoded_X]
+files = ['img/'+str for str in sorted(os.listdir('img/'))]
+
+img_raw = [tf.read_file(file) for file in files]
+img_decoded = [tf.cast(tf.image.decode_png(i),tf.float32)/(2**BIT_DEPTH) for i in img_raw]
+
+img_resized_X = [
+    tf.image.resize_images(
+        tf.image.resize_images(i,[tf.shape(img_decoded[0])[0]//RESIZE_K,tf.shape(img_decoded[0])[1]//RESIZE_K]),
+        [tf.shape(img_decoded[0])[0],tf.shape(img_decoded[0])[1]]
+    )
+    for i in img_decoded]
+
+l1 = Layers.patch_extraction(x=img_resized_X,channels=4)
+l2 = Layers.none_linear_mapping(x=l1.out,channels=4)
+l3 = Layers.reconstruction(x=l2.out,channels=4)
+
+cost = tf.reduce_mean(tf.squared_difference(l3.out, img_decoded))
+optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
+train_op = optimizer.minimize(loss=cost,var_list=[l1.W,l1.B,l2.W,l2.B,l3.W,l3.B])
 
 init = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init)
 
-i1 = sess.run(img_decoded_X)
-i2 = sess.run(img_resized_X)
-i3 = sess.run(img_decoded_Y)
-print(np.shape(i1))
-print(np.shape(i2))
-print(np.shape(i3))
-print(psnr.psnr(i2[0], i3[0]))
-print(psnr.psnr(i3[0], i3[0]))
-fig=plt.figure()
-fig.add_subplot(3,1,1)
-plt.imshow(i1[0])
-fig.add_subplot(3,1,2)
-plt.imshow(i2[0])
-fig.add_subplot(3,1,3)
-plt.imshow(i3[0])
-plt.show()
+i = 0
+while True:
+    if i%100 == 0:
+        print(sess.run(cost))
+        i1 = np.array(sess.run(img_decoded))
+        i2 = np.array(sess.run(l3.out))
+        #print(i2)
+        fig=plt.figure()
+        fig.add_subplot(2,1,1)
+        plt.imshow(np.reshape(i1[0,:,:,0:3],[92,100,3]))
+        fig.add_subplot(2,1,2)
+        plt.imshow(np.reshape(i2[0,:,:,0:3],[92,100,3]))
+        plt.pause(0.5)
+        plt.close()
+    i = i+1
+    sess.run(train_op)
+
+
+
 
--- src/Layers.py
+++ src/Layers.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 
 class patch_extraction(object):
-    def __init__(self,x,kernel_size=5,strides=1,channels=3,W=None,B=None):
+    def __init__(self,x,kernel_size=9,strides=1,channels=3,W=None,B=None):
         if W is None:
-            self.W = tf.Variable(tf.random_normal([kernel_size,kernel_size,channels,channels]))
+            #self.W = tf.Variable(tf.random_uniform(shape=[kernel_size,kernel_size,channels,channels],minval=0,maxval=1))
+            self.W = tf.Variable(tf.random_normal(shape=[kernel_size,kernel_size,channels,channels]))
         else:
             self.W = W
         if B is None:
@@ -13,3 +14,39 @@ class patch_extraction(object):
         y = tf.nn.conv2d(x, self.W, strides=[1, strides, strides, 1], padding='SAME')
         y = tf.nn.bias_add(y, self.B)
         self.out = tf.nn.relu(y)
+        
+class none_linear_mapping(object):
+    def __init__(self,x,kernel_size=1,strides=1,channels=3,W=None,B=None):
+        if W is None:
+            #self.W = tf.Variable(tf.random_uniform(shape=[kernel_size,kernel_size,channels,channels],minval=0,maxval=1))
+            self.W = tf.Variable(tf.random_normal(shape=[kernel_size,kernel_size,channels,channels]))
+        else:
+            self.W = W
+        if B is None:
+            self.B = tf.Variable(tf.zeros([channels]))
+        else:
+            self.B = B
+        y = tf.nn.conv2d(x, self.W, strides=[1, strides, strides, 1], padding='SAME')
+        y = tf.nn.bias_add(y, self.B)
+        self.out = tf.nn.relu(y)
+
+class reconstruction(object):
+    def __init__(self,x,kernel_size=5,strides=1,channels=3,W=None,B=None):
+        if W is None:
+            #self.W = tf.Variable(tf.random_uniform(shape=[kernel_size,kernel_size,channels,channels],minval=0,maxval=1))
+            self.W = tf.Variable(tf.random_normal(shape=[kernel_size,kernel_size,channels,channels]))
+        else:
+            self.W = W
+        if B is None:
+            self.B = tf.Variable(tf.zeros([channels]))
+        else:
+            self.B = B
+        y = tf.nn.conv2d(x, self.W, strides=[1, strides, strides, 1], padding='SAME')
+        y = tf.nn.bias_add(y, self.B)
+        '''
+        ls0 = tf.less(y,0)
+        gt1 = tf.greater(y,1)
+        y = tf.where(ls0,tf.zeros_like(y),y)
+        y = tf.where(gt1,tf.ones_like(y),y)
+        '''
+        self.out = tf.nn.sigmoid(y)
