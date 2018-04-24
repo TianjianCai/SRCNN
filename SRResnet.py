9c5e653fbab2073c17ebd42d0c5803b8695c3853
Tianjian Cai <tjcaiforapp@gmail.com>
2018-04-23 21:25:13 -0400

    final ver

--- SRResnet.py
+++ SRResnet.py
@@ -12,7 +12,7 @@ class ResUnit(object):
                  keep_prob=1,
                  training=True,
                  w1=None, w2=None, w3=None, b1=None, b2=None, b3=None,
-                 k1=9, k2=1, k3=7, c1=3, c2=64, c3=32):
+                 k1=9, k2=3, k3=5, c1=3, c2=64, c3=32):
         if w1 is None:
             self.w1 = tf.Variable(tf.random_normal(shape=[k1, k1, c1, c2]))
             self.w2 = tf.Variable(tf.random_normal(shape=[k2, k2, c2, c3]))
@@ -58,12 +58,12 @@ class ResUnit(object):
 
 BIT_DEPTH = 8
 RESIZE_K = 2
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 1e-6
 DROPOUT = 0.75
-BATCH_SIZE = 4
+BATCH_SIZE = 6
 K = 100
 SHOW_PLT = False
-RESNUM = 10
+RESNUM = 3
 ITERATION_NUM = 1000
 
 files = ['img/' + str for str in sorted(os.listdir('img/'))]
@@ -74,13 +74,17 @@ keep_prob = tf.placeholder(tf.float32)
 batch_start = tf.placeholder(tf.int32)
 batch_size = tf.placeholder(tf.int32)
 is_training = tf.placeholder(tf.bool)
+shuffle = tf.placeholder(tf.bool)
 
 iteration_count = tf.Variable(0, dtype=tf.int64)
 iteration_add = tf.assign(iteration_count,iteration_count+ITERATION_NUM)
 
 img_raw = tf.stack([tf.read_file(file) for file in files])
+img_raw_not_shuffled = img_raw
 
-img_raw_batch = tf.slice(img_raw, [batch_start], [batch_size])
+shuffle_op = tf.random_shuffle(img_raw)
+
+img_raw_batch = tf.slice(tf.cond(shuffle,lambda: img_raw, lambda: img_raw_not_shuffled), [batch_start], [batch_size])
 
 
 # img_decoded = [tf.image.rgb_to_hsv(tf.cast(tf.image.decode_jpeg(i),tf.float32)/(2**BIT_DEPTH)) for i in img_raw]
@@ -112,14 +116,20 @@ img_resized_X = tf.reshape(img_resized_X,[-1,tf.shape(img_decoded)[1],tf.shape(i
 
 
 reslist = []
-reslist.append(ResUnit(x=img_resized_X))
+reslist.append(ResUnit(x=img_resized_X2))
 cost = reslist[0].weight_cost
 for i in range(1, RESNUM):
     reslist.append(ResUnit(x=reslist[i-1].UnitOut))
     cost = tf.add(cost, reslist[i].weight_cost)
 resout = reslist[len(reslist)-1]
 
-cost = cost + tf.reduce_mean(tf.square(resout.UnitOut - img_decoded))
+cost_entropy = tf.reduce_mean(tf.clip_by_value(tf.subtract(0., tf.log(tf.clip_by_value(tf.subtract(1., tf.abs(tf.subtract(img_decoded, resout.UnitOut))), 1e-5, 1.))), 0., 1e5))
+
+cost_old = cost + tf.reduce_mean(tf.square(resout.UnitOut - img_decoded))
+cost = cost + cost_entropy
+
+input_mse = tf.reduce_mean(tf.square(img_resized_X - img_decoded))
+default_mse = tf.reduce_mean(tf.square(img_resized_X2 - img_decoded))
 
 optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
 train_op = optimizer.minimize(loss=cost)
@@ -141,8 +151,7 @@ try:
 except:
     print('cannot load checkpoint')
 
-print(sess.run(tf.reduce_mean(tf.square(img_resized_X2 - img_decoded)), {batch_start: 1, batch_size: 1}))
-print(sess.run(tf.reduce_mean(tf.square(img_resized_X - img_decoded)), {batch_start: 1, batch_size: 1}))
+print(sess.run([input_mse, default_mse], {batch_start: 5, batch_size: 1, shuffle: False}))
 
 if SHOW_PLT is True:
     plt.ion()
@@ -150,8 +159,8 @@ if SHOW_PLT is True:
     p1 = fig.add_subplot(1, 3, 1)
     p2 = fig.add_subplot(1, 3, 2)
     p3 = fig.add_subplot(1, 3, 3)
-    p1.imshow(sess.run(img_decoded, {batch_start: 1, batch_size: 1})[0], interpolation='none')
-    p3.imshow(sess.run(img_resized_X, {batch_start: 1, batch_size: 1})[0], interpolation='none')
+    p1.imshow(sess.run(img_decoded, {batch_start: 5, batch_size: 1, shuffle: False})[0], interpolation='none')
+    p3.imshow(sess.run(img_resized_X, {batch_start: 5, batch_size: 1, shuffle: False})[0], interpolation='none')
 
 i = 0
 file_start = 0
@@ -159,11 +168,14 @@ file_size = BATCH_SIZE
 while True:
 
     if i % ITERATION_NUM == 0:
+        sess.run(shuffle_op, {shuffle: False})
         saver.save(sess, 'C:\\Users\\CTJ\\Documents\\GitHub\\SRCNN\\log\\resnet.ckpt')
-        c = sess.run(cost, {keep_prob: 1, batch_start: 1, batch_size: 1, is_training: False})
-        c2 = sess.run(resout.weight_cost, {keep_prob: 1, batch_start: 1, batch_size: 1, is_training: False})
-        print('iteration ' + repr(sess.run(iteration_count)) + ', cost: ' + repr(c) + ', weight cost: ' + repr(c2))
-        i2 = sess.run(resout.UnitOut, {keep_prob: 1, batch_start: 1, batch_size: 1, is_training: False})
+        c = sess.run(cost, {keep_prob: 1, batch_start: 5, batch_size: 1, is_training: False, shuffle: False})
+        c2 = sess.run(resout.weight_cost, {keep_prob: 1, batch_start: 5, batch_size: 1, is_training: False, shuffle: False})
+        ce = sess.run(cost_entropy, {keep_prob: 1, batch_start: 5, batch_size: 1, is_training: False, shuffle: False})
+        mse = sess.run(cost_old, {keep_prob: 1, batch_start: 5, batch_size: 1, is_training: False, shuffle: False})
+        print('iteration ' + repr(sess.run(iteration_count)) + ', cost: ' + repr(mse) + ', weight cost: ' + repr(c2) + ', entropy: ' + repr(ce))
+        i2 = sess.run(resout.UnitOut, {keep_prob: 1, batch_start: 5, batch_size: 1, is_training: False, shuffle:False})
         #print(sess.run(resout.addition[0], {keep_prob: 1, batch_start: 1, batch_size: 1, is_training: False}))
         with open('mse.csv','a+') as f:
             content = repr(sess.run(iteration_count)) + ',' + repr(c) + '\n'
@@ -180,7 +192,8 @@ while True:
             fig.canvas.draw()
             plt.pause(0.1)
     i = i + 1
-    sess.run(train_op, {keep_prob: DROPOUT, batch_start: file_start, batch_size: file_size, is_training: True})
+    sess.run(shuffle_op, {shuffle: True})
+    sess.run(train_op, {keep_prob: DROPOUT, batch_start: file_start, batch_size: file_size, shuffle: True})
     file_start = file_start + BATCH_SIZE
     if file_start + file_size > file_length - 1:
         file_start = 0
